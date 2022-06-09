df8f10895348c8f92cf531a628ae4f9518cc6997
Kishore Kumar <kishore.sah@rsystems.com>
2022-06-09 13:12:03 +0530

    more row added2

--- arrow.js
+++ arrow.js
@@ -16,3 +16,10 @@ arrays.push("kishore4");
 arrays.push("kishore3");
 arrays.push("kishore2");
 console.log(arrays1)
+
+const arrays2=[];
+arrays.push("kishore");
+arrays.push("kishore4");
+arrays.push("kishore3");
+arrays.push("kishore2");
+console.log(arrays2)
