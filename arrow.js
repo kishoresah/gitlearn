f507d0bad1e5b3378ac2ecb687b55ac6298b4424
Kishore Kumar <kishore.sah@rsystems.com>
2022-06-09 13:07:46 +0530

    arrow.js

--- arrow.js
+++ arrow.js
@@ -2,3 +2,10 @@ const getFirstName=()=>{
     return "kishore sah";
 }
 console.log(getFirstName());
+
+const arrays=[];
+arrays.push("kishore");
+arrays.push("kishore4");
+arrays.push("kishore3");
+arrays.push("kishore2");
+console.log(arrays)
